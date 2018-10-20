e80b742bb41cb9f4d2e05d9b8b968ad902adc54e
Mrrrrr10 <104758993@qq.com>
2018-10-20 21:25:43 +0800

    Update items.py

--- WeiboList_Spider/items.py
+++ WeiboList_Spider/items.py
@@ -14,62 +14,6 @@ class WeiboListSpiderItem(scrapy.Item):
     pass
 
 
-# ---------- Item_WeiboTopic ----------
-class WeiboHotSearchItem(scrapy.Item):
-    topic_name = scrapy.Field()
-    total = scrapy.Field()
-    start_time = scrapy.Field()
-    id = scrapy.Field()
-    weibo_url = scrapy.Field()
-    text = scrapy.Field()
-    source = scrapy.Field()
-    created_at = scrapy.Field()
-    reposts_count = scrapy.Field()  # 转发
-    comments_count = scrapy.Field()  # 评论
-    attitudes_count = scrapy.Field()  # 点赞
-    review_count = scrapy.Field()
-    video_link = scrapy.Field()
-    user_id = scrapy.Field()
-    username = scrapy.Field()
-    gender = scrapy.Field()
-    statuses_count = scrapy.Field()
-    verified_reason = scrapy.Field()
-    followers_count = scrapy.Field()
-    follow_count = scrapy.Field()
-    description = scrapy.Field()
-    comment_info = scrapy.Field()
-    comment_list = scrapy.Field()
-    crawled_at = scrapy.Field()
-
-
-class WeiboTopicListItem(scrapy.Item):
-    topic_name = scrapy.Field()
-    reading_volume = scrapy.Field()
-    total = scrapy.Field()
-    start_time = scrapy.Field()
-    id = scrapy.Field()
-    weibo_url = scrapy.Field()
-    text = scrapy.Field()
-    source = scrapy.Field()
-    created_at = scrapy.Field()
-    reposts_count = scrapy.Field()  # 转发
-    comments_count = scrapy.Field()  # 评论
-    attitudes_count = scrapy.Field()  # 点赞
-    review_count = scrapy.Field()
-    video_link = scrapy.Field()
-    user_id = scrapy.Field()
-    username = scrapy.Field()
-    gender = scrapy.Field()
-    statuses_count = scrapy.Field()
-    verified_reason = scrapy.Field()
-    followers_count = scrapy.Field()
-    follow_count = scrapy.Field()
-    description = scrapy.Field()
-    comment_info = scrapy.Field()
-    comment_list = scrapy.Field()
-    crawled_at = scrapy.Field()
-
-
 # ---------- Item_WeiboUser ----------
 class WeiboUserItem(scrapy.Item):
     id = scrapy.Field()
